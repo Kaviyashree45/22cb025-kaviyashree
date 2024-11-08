77b51315815db9429b68de0fd6e5c7efc1eea1e2
Kaviyashree45 <119500173+Kaviyashree45@users.noreply.github.com>
2024-11-08 14:19:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World -Kaviya Shree G T-22CB025
+         Hello World -KaviyaShreeGT-22CB025
         </p>
         <a
           className="App-link"
